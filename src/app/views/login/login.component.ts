1feb33b3fef182f7f0a450ba260be9a76941d294
Rodrigo Oliveira <rodrigooliveira@rodrigo-2.home>
2019-02-07 07:46:25 -0200

    obtendo token para request

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -9,17 +10,50 @@ import { HttpClient } from '@angular/common/http'
 })
 export class LoginComponent implements OnInit {
 
-  public code: string = null
+  private code: string = null
+  github: any = environment.github;
 
-  constructor(private routeParam: ActivatedRoute, private api: HttpClient) { }
+  constructor(private routeParam: ActivatedRoute, public api: HttpClient) { }
 
   ngOnInit() {
+    // this.api.post(`https://api.github.com/authorizations/clients/${this.github.clientId}`, {
+    //   client_secret: this.github.clientSecret,
+    //   scope: ['user_email', 'public_repo', 'private_repo'],
+    //   note: 'Pan app'
+    // })
+    // .subscribe(resp => {
+    //   console.log(resp)
+    // })
+
+    
+
     this.routeParam.queryParams.subscribe(query => {
       this.code = query.code;
+      console.log(query)
       if (this.code){
-        console.log(this.code)
+        this.getToken(this.code).subscribe(respo => {
+          console.log(respo)
+        })
       }
     })
+  }
+
+  getToken(clientCode){
+
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      })
+    }
+
+    const credentials = {
+      client_id: this.github.clientId,
+      client_secret: this.github.clientSecret,
+      code: clientCode
+    }
+
+    return this.api.post(`https://github.com/login/oauth/access_token`, credentials, options)
   }
 
 }
